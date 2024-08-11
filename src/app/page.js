9bfee10705b9e0d0665cc88aec5903f729f06051
rashirashi12122001 <rashiarora12122001@gmail.com>
2024-08-12 02:39:28 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import bg from "../../public/background/bg_home_3.jpg";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image
+        priority
+        sizes="100vw"
+        src={bg}
+        alt="background-image"
+        fill
+        className="-z-50 w-full h-full object-cover object-center opacity-60"
+      />
+      <div className="w-full h-screen">
+        <Navigation />
+        <RenderModel>
+          <Wizard />
+        </RenderModel>
+      </div>
+    </main>
+  );
+}
